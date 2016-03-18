325f600dc584150f4d52374898883ac1e524fe3d
Chyzwar <schizek.marcin@gmail.com>
2016-03-18 17:07:57 +0000

    initial config

---
+++ examples/isomorphic/webpackfile.js
@@ -0,0 +1,71 @@
+import webpack from 'webpack';
+
+const plugins = [
+  new webpack.DefinePlugin({ 'process.env.NODE_ENV': 'development' }),
+];
+
+/**
+ * Configuration for client webpack (development)
+ * @type {Object}
+ */
+const client = {
+  entry: {
+    client: './src/client.js',
+  },
+  output: {
+    path: './build/public/',
+    filename: 'client.js',
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['', '.js', '.jsx'],
+  },
+  target: 'web',
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015'],
+      },
+    }],
+  },
+  cache: true,
+  plugins,
+  devtool: 'cheap-module-eval-source-map',
+};
+
+
+/**
+ * Configuration for server webpack
+ * @type {Object}
+ */
+const server = {
+  entry: {
+    server: './src/server.js',
+  },
+  output: {
+    path: './build/',
+    filename: 'server.js',
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['', '.js', '.jsx'],
+  },
+  target: 'node',
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loader: 'babel',
+      query: {
+        presets: ['node5'],
+      },
+    }],
+  },
+  cache: true,
+  plugins,
+  devtool: 'cheap-module-eval-source-map',
+};
+
+
+export default [client, server];
