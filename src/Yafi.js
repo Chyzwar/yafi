f89d56335c59b395f344aab55485fab1a411c616
Chyzwar <schizek.marcin@gmail.com>
2016-04-19 07:34:13 +0100

    yafi rename index

---
+++ src/Yafi.js
@@ -0,0 +1,7 @@
+/**
+ * Reexport public classes.
+ */
+export { default as Action } from './Action';
+export { default as Container } from './Container';
+export { default as Dispatcher } from './Dispatcher';
+export { default as Store } from './Store';
